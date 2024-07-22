22b523d66780dbe51d39503e625ecbac82de5b48
Phil Jirsa <phil.jirsa@microsoft.com>
2024-07-22 11:46:41 -0500

    chore: Add IndexOrchestration API endpoints and models

---
+++ src/IndexOrchestration/IndexFields.cs
@@ -0,0 +1,26 @@
+﻿namespace Api;
+
+internal static class IndexFields
+{
+    public const string IndexRecordId = "IndexRecordId";
+    public const string NoteId = "NoteId";
+    public const string NoteChunk = "NoteChunk";
+    public const string NoteChunkOrder = "NoteChunkOrder";
+    public const string NoteChunkVector = "NoteChunkVector";
+    public const string CSN = "CSN";
+    public const string MRN = "MRN";
+    public const string NoteType = "NoteType";
+    public const string NoteStatus = "NoteStatus";
+    public const string AuthorId = "AuthorId";
+    public const string PatientFirstName = "PatientFirstName";
+    public const string PatientLastName = "PatientLastName";
+    public const string AuthorFirstName = "AuthorFirstName";
+    public const string AuthorLastName = "AuthorLastName";
+    public const string Department = "Department";
+    public const string Gender = "Gender";
+    public const string BirthDate = "BirthDate";
+
+    public const string FilePath = "FilePath";
+    public const string Title = "Title";
+    public const string Url = "Url";
+}
