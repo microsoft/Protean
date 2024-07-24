89ef69b34e6e65cbc9bb9d3f652d67a9e658ec05
cajetzer <cajetzer@microsoft.com>
2024-07-24 14:31:39 -0400

    remove search and update system prompt to return to basic orchestration with LLM and nothing else.

--- src/ChatApp/ChatApp.Server/Endpoints.Api.cs
+++ src/ChatApp/ChatApp.Server/Endpoints.Api.cs
@@ -31,24 +31,24 @@ public static partial class Endpoints
 
     private static async Task<IResult> PostConversationAsync(
         [FromServices] ChatCompletionService chat,
-        [FromServices] AzureSearchService search,
+        //[FromServices] AzureSearchService search,
         [FromBody] ConversationRequest history)
     {
         // filter out any existing tool messages (search results)
-        history.Messages = history.Messages.Where(m => !m.Role.Equals(AuthorRole.Tool.ToString(), StringComparison.OrdinalIgnoreCase)).ToList();
+        // history.Messages = history.Messages.Where(m => !m.Role.Equals(AuthorRole.Tool.ToString(), StringComparison.OrdinalIgnoreCase)).ToList();
         // do the search here
-        var searchResults = await search.QueryDocumentsAsync(history.Messages[^1].Content);
-        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-        var toolMsg = new Message
-        {
-            Id = Guid.NewGuid().ToString(),
-            Role = AuthorRole.Tool.ToString().ToLower(),
-            Date = DateTime.UtcNow,
-            Content = JsonSerializer.Serialize<ToolContentResponse>(searchResults, options)
-        };
-        // add search results to the conversation
-        history.Messages.Add(toolMsg);
-        
+        //var searchResults = await search.QueryDocumentsAsync(history.Messages[^1].Content);
+        //var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        //var toolMsg = new Message
+        //{
+        //    Id = Guid.NewGuid().ToString(),
+        //    Role = AuthorRole.Tool.ToString().ToLower(),
+        //    Date = DateTime.UtcNow,
+        //    Content = JsonSerializer.Serialize<ToolContentResponse>(searchResults, options)
+        //};
+        //// add search results to the conversation
+        //history.Messages.Add(toolMsg);
+
         // return result from LLM
         return Results.Ok(await chat.CompleteChat([.. history.Messages]));
     }
